319a1a223ce1256cf926ddcd63ba2623ef8400cd
lazzzolvan <MR.Mersen@yandex.ru>
2023-09-26 22:36:28 +0300

    Initial commit

---
+++ Lab1_1/Lab1_1.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Lab1
+{
+
+    class Lab1_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку для проверки:");
+            string input = Console.ReadLine();
+
+            string cleanedInput = RemoveSpacesAndToLower(input);
+
+            bool isPalindrome = IsPalindrome(cleanedInput);
+
+            if (isPalindrome)
+            {
+                Console.WriteLine("Это палиндром!");
+            }
+            else
+            {
+                Console.WriteLine("Это не палиндром.");
+            }
+        }
+
+        static string RemoveSpacesAndToLower(string str)
+        {
+            return str.Replace(" ", "").Replace(",", "")
+                .Replace("?", "").Replace("!", "")
+                .Replace(".", "").ToLower();
+        }
+
+        static bool IsPalindrome(string str)
+        {
+            for (int i = 0; i < str.Length/ 2; i++)
+            {
+                if (str[i] != str[str.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
---
+++ Lab1_2/Lab1_2.cs
@@ -0,0 +1,76 @@
+﻿namespace Lab1_2;
+
+public class Lab1_2
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк матрицы: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов матрицы: ");
+        int columns = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[rows, columns];
+
+        Random random = new Random();
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                matrix[i, j] = random.Next(-10, 10); // Генерируем случайные числа от -10 до 10
+            }
+        }
+        
+        PrintMatrix(matrix);
+
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zeroCount = 0;
+
+        string zeroCoordinates = "";
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                int currentElement = matrix[i, j];
+                if (currentElement > 0)
+                {
+                    positiveCount++;
+                }
+                else if (currentElement < 0)
+                {
+                    negativeCount++;
+                }
+                else
+                {
+                    zeroCount++;
+                    zeroCoordinates += $"({i}, {j}) ";
+                }
+            }
+        }
+
+        Console.WriteLine($"Количество положительных элементов: {positiveCount}");
+        Console.WriteLine($"Количество отрицательных элементов: {negativeCount}");
+        Console.WriteLine($"Количество нулевых элементов: {zeroCount}");
+        if (zeroCount > 0)
+        {
+            Console.WriteLine($"Координаты нулевых элементов: {zeroCoordinates}");
+        }
+    }
+    
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
