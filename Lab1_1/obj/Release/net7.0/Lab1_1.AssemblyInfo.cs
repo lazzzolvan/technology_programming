76624c5cbd26e694d1e807b2e7dd2cd5662a0587
SenkinSer <sergey.senkin@inbox.ru>
2023-09-27 17:57:26 +0300

    фоапрл

--- Lab1_1/obj/Release/net7.0/Lab1_1.AssemblyInfo.cs
+++ Lab1_1/obj/Release/net7.0/Lab1_1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lab1_2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lab1_1")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lab1_2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lab1_2")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lab1_1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Lab1_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
